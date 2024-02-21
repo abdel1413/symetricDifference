981fbf324b9ee1f77cb5dd2908f68fcfb4400366
Your Name <you@example.com>
2024-02-21 09:24:02 -0500

    debugging

--- numPermutation.js
+++ numPermutation.js
@@ -20,21 +20,21 @@ const numPermutation = (numArray) => {
     //loop thru the array and call swap at each index
     //if even, swape with i else swape with 0
     for (let i = 0; i < int - 1; i++) {
-      //   int % 2 == 0
-      //     ? swapper(heapArr, i, int - 1)
-      //     : swapper(heapArr, 0, int - 1);
-      if (int % 2 === 0) {
-        swapper(heapArr, i, int - 1);
-      } else {
-        swapper(heapArr, 0, int - 1);
-      }
+      //   if (int % 2 === 0) {
+      //     swapper(heapArr, i, int - 1);
+      //   } else {
+      //     swapper(heapArr, 0, int - 1);
+      //   }
+
+      int % 2 == 0
+        ? swapper(heapArr, i, int - 1)
+        : swapper(heapArr, 0, int - 1);
       generate(int - 1, heapArr);
     }
-
-    generate(numArray.length, numArray.slice());
-    console.log("result");
-    return result;
   };
+  generate(numArray.length, numArray.slice());
+  console.log("result");
+  return result;
 };
 
 console.log(numPermutation([1, 2]));
