81ed6931cac2791cce35d9673eca2ab31385915d
Your Name <you@example.com>
2024-02-22 12:17:03 -0500

    selection sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -16,3 +16,7 @@ const bubbleSort = (array) => {
   } while (swapped);
   return array;
 };
+
+bubbleSort([
+  1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92,
+]);
---
+++ selectionSort.js
@@ -0,0 +1,26 @@
+const selectionSort = (array) => {
+  //create a swapper function that swapes the least
+  //and greates element in  array
+  const swapper = (index0, index1, array) => {
+    let temp = array[index0];
+    array[index0] = array[index1];
+    array[index1] = temp;
+  };
+  // set the min to each index in array
+  let min;
+  for (let i = 0; i < array.length; i++) {
+    min = i;
+
+    //find the min in the inner loop
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] > array[min]) {
+        min = j;
+      }
+
+      //call swap function
+      swapper(i, min, array);
+    }
+  }
+
+  return array;
+};
