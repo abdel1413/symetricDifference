65bd1f1471cc3fc2aaa154e54bdadac67efa4454
Your Name <you@example.com>
2024-04-29 16:35:10 -0400

    depth frist search

---
+++ dataStructure/Graphs/depthFirsSearch.js
@@ -0,0 +1,59 @@
+//1 create a function that takes grapth and root as params
+//2 : creat a stack to push nodes/vertices
+//3: create a visited array to push the nodes that are alred visited
+//4:  create a array to push neighbors vertices
+// 5 : wile stack has length;
+// a -pop element from stack
+// b- check if the element popped from stack exist in the visited array
+// if not push it in the visited array (use .indexOf()methd)
+// c - set tempNeighbor to graph elemets;
+// 6:  loop thru tempNeigbor
+// a: push the insex to stack if any value is equal to 1;
+// 7: return visited aray
+function depthFirstSearch(graph, root) {
+  let stack = [];
+  let visited = [];
+  let tempNeighbors = [];
+
+  stack.push(root);
+  while (stack.length) {
+    let tempNode = stack.pop();
+
+    if (visited.indexOf(tempNode) == -1) {
+      visited.push(tempNode);
+      tempNeighbors = graph[tempNode];
+      for (let i = 0; i < tempNeighbors.length; i++) {
+        if (tempNeighbors[i] === 1) {
+          stack.push(i);
+        }
+      }
+    }
+  }
+  return visited;
+}
+
+var exDFSGraph = [
+  [0, 1, 0, 0],
+  [1, 0, 1, 0],
+  [0, 1, 0, 1],
+  [0, 0, 1, 0],
+];
+
+var d = [
+  [0, 1, 0, 0],
+  [1, 0, 1, 0],
+  [0, 1, 0, 1],
+  [0, 0, 1, 0],
+];
+var c = [
+  [0, 1, 0, 0],
+  [1, 0, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 1, 0],
+];
+console.log(depthFirstSearch(exDFSGraph, 3));
+// =>[ 3, 2, 1, 0 ]
+console.log(depthFirstSearch(d, 1));
+//=>[ 1, 2, 3, 0 ]
+console.log(depthFirstSearch(c, 3));
+//=>[ 3, 2 ]
